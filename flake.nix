e74d1964593f5fe61c75cc45b61cde74a89384e2
Mikilio <official.mikilio@gmail.com>
2023-08-02 21:35:41 +0200

    feat: Nixify

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Utility to allow streaming Wayland windows to X applications";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, flake-utils, nixpkgs }:
+    let
+
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+    in rec {
+
+      packages.x86_64-linux.xwaylandvideobridge = pkgs.libsForQt5.callPackage ./. { };
+      packages.x86_64-linux.default = packages.x86_64-linux.xwaylandvideobridge;
+
+      devShells.x86_64-linux.default = with pkgs;
+        mkShell {
+          buildInputs = [
+            cmake
+            extra-cmake-modules
+            libsForQt5.qt5.wrapQtAppsHook
+            libsForQt5.qt5.qtbase
+            libsForQt5.qt5.qtdeclarative
+            libsForQt5.qt5.qtx11extras
+            libsForQt5.kpipewire
+            libsForQt5.ki18n
+            libsForQt5.knotifications
+            libsForQt5.kwidgetsaddons
+            libsForQt5.kwindowsystem
+            xorg.libxcb
+            pkg-config
+          ];
+
+          shellHook = ''
+            export QT_QPA_PLATFORM_PLUGIN_PATH="${qt5.qtbase.bin}/lib/qt-${qt5.qtbase.version}/plugins/platforms";
+          '';
+        };
+    };
+}
